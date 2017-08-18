f358d5c16df9db13af6cfe2a3891b34eb5ac25ae
test@gmail.com <test@gmail.com>
2017-08-18 13:16:25 +0530

    Initial commit

---
+++ src/main/java/com/userinfo/domain/User.java
@@ -0,0 +1,51 @@
+package com.userinfo.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the users database table.
+ * 
+ */
+@Entity
+@Table(name="users")
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String username;
+
+	private byte enabled;
+
+	private String password;
+
+	public User() {
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public byte getEnabled() {
+		return this.enabled;
+	}
+
+	public void setEnabled(byte enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
